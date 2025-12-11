2604a20323baddf0aec4cff879b16a043bffbbfc
tobiasxcz <czarnecki.tristan@gmail.com>
2025-12-11 20:45:14 +0100

    correction main

--- main_CDFT_uni.py
+++ main_CDFT_uni.py
@@ -19,31 +19,23 @@ modele_fut = pd.read_csv("data/CMIP5_rcp85.csv", sep=';', parse_dates=["date"])
 
 
 """ Filtrage par mois  -- exemple pour Juillet """
-
 jul_obs = obs[obs['date'].dt.month == 7]
 jul_mod_hist = modele_hist[modele_hist['date'].dt.month == 7]
 jul_mod_fut = modele_fut[modele_fut['date'].dt.month == 7]
+EUROCORDEX = pd.read_csv("data/EUROCORDEX_historical.csv", sep=';', parse_dates=["date"])
+EUROCORDEX_jul = EUROCORDEX[EUROCORDEX['date'].dt.month == 7] 
+EUROCORDEX_jul_tas = EUROCORDEX_jul['tas'].values  
+# on garde que les températures pour être homogène avec les CFD-t qui sont aussi des tableaux 1D ==> ensuite fdr en bien
 
 
-
-
-""" Boucle des mois  -- Dictionnaires """
-
-obs_mois = {}
-modele_hist_mois = {}
-modele_fut_mois = {}
-for i in range(1, 13):
-    obs_mois[i] = obs[obs['date'].dt.month == i]
-    modele_hist_mois[i] = modele_hist[modele_hist['date'].dt.month == i]
-    modele_fut_mois[i] = modele_fut[modele_fut['date'].dt.month == i]
 
 
 """ FDR par mois  """
-
 plt.figure(figsize=(10,6))
 ax = plt.gca()
 couleurs = plt.cm.tab20.colors  # 20 couleurs possibles
 for i in range(1, 13):
+    obs_mois = obs[obs['date'].dt.month == i]
     tas_mois = obs_mois[i]['tas']  # <- on prend uniquement la colonne des températures
     graph_fdr_12(tas_mois, ax=ax, show_gaussian=False, label=f"Mois {i}", couleur=couleurs[i-1])
 ax.legend()
@@ -65,11 +57,7 @@ ax.legend()
 cdft_juillet_fut = cdf_t_correction(jul_mod_hist['tas'], jul_obs['tas'], jul_mod_fut['tas'])
 cdft_juillet_hist = cdf_t_correction(jul_mod_hist['tas'], jul_obs['tas'], jul_mod_hist['tas'])
 
-""" EUROCORDEX Juillet : correction températures"""
-EUROCORDEX = pd.read_csv("data/EUROCORDEX_historical.csv", sep=';', parse_dates=["date"])
-EUROCORDEX_jul = EUROCORDEX[EUROCORDEX['date'].dt.month == 7] 
-EUROCORDEX_jul_tas = EUROCORDEX_jul['tas'].values  
-# on garde que les températures pour être homogène avec les CFD-t qui sont aussi des tableaux 1D ==> ensuite fdr en bien
+
 
 
 
@@ -87,4 +75,18 @@ for i in range(1, 13):
     modele_fut_mois[i] = modele_fut[modele_fut['date'].dt.month == i]
     EUROCORDEX_mois = EUROCORDEX[EUROCORDEX['date'].dt.month == i] 
     EUROCORDEX_mois_tas = EUROCORDEX_mois['tas'].values
-    plot_cdf_t(EUROCORDEX_mois_tas, cdf_t_correction(modele_hist_mois[i]['tas'], obs_mois[i]['tas'], modele_hist_mois[i]['tas']), cdf_t_correction(modele_hist_mois[i]['tas'], obs_mois[i]['tas'], modele_hist_mois[i]['tas']), cdf_t_correction(modele_hist_mois[i]['tas'], obs_mois[i]['tas'], modele_fut_mois[i]['tas']))
+    plot_cdf_t(EUROCORDEX_mois_tas, cdf_t_correction(modele_hist_mois[i]['tas'], EUROCORDEX_mois_tas, modele_hist_mois[i]['tas']), cdf_t_correction(modele_hist_mois[i]['tas'], obs_mois[i]['tas'], modele_hist_mois[i]['tas']), cdf_t_correction(modele_hist_mois[i]['tas'], obs_mois[i]['tas'], modele_fut_mois[i]['tas']))
+
+
+
+mod_hist_corr = cdf_t_correction(modele_hist_mois[1]['tas'], obs_mois[1]['tas'], modele_hist_mois[1]['tas'])
+fut_corr      = cdf_t_correction(modele_hist_mois[1]['tas'], obs_mois[1]['tas'], modele_fut_mois[1]['tas'])
+EUROCORDEX_mois = EUROCORDEX[EUROCORDEX['date'].dt.month == 1]
+EUROCORDEX_mois_tas = EUROCORDEX_mois['tas'].values
+
+plot_cdf_t(
+    EUROCORDEX_mois_tas,  # brut
+    mod_hist_corr,         # CMIP5 historique corrigé
+    obs_mois[1]['tas'],    # observations
+    fut_corr               # CMIP5 futur corrigé
+)
